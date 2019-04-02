4c7102f6a4b64e7326399fd7b31e688546b163ed
konradcinkusz <34044180+konradcinkusz@users.noreply.github.com>
2019-04-02 13:19:39 +0200

    Update NumpyArrayWithoutSpecificValue.py

--- Common/NumpyArrayWithoutSpecificValue.py
+++ Common/NumpyArrayWithoutSpecificValue.py
@@ -5,3 +5,5 @@ a = np.arange(-5,6)
 b = np.where([np.arange(-5,6) != 0])[1]
 c = numpy.where(numpy.in1d(a, [0]))
 d = random.choice(list(numpy.select([a != 0, a == 0], [a, -1])))
+# to generate matrix 5 x 5:
+e = np.random.choice(list(numpy.select([a != 0, a == 0], [a, -1])), (5, 5))
