081fb1fe95baccc9df65cb8b6779ea18802e300d
konradcinkusz <34044180+konradcinkusz@users.noreply.github.com>
2019-04-24 15:29:44 +0200

    Create Tensors.py

---
+++ Common/Tensors.py
@@ -0,0 +1,9 @@
+import numpy as np
+#tensor zerowymiarowy
+np.array(12)
+#tensor jednowymiarowy - wektor szesciowymiarowy
+np.array([43,5,3,5,6,9])
+#tensor dwywymiarowy (dwie osie wiersze i kolumny) - macierz
+np.array([43,5,3,5,6,9],[43,5,3,5,6,9])
+#tensor trójwymiarowy - sześcian liczbowy i tak dalej z n wymiarowymi tensorami
+np.array([[[43,5,3,5,6,9],[43,5,3,5,6,9]],[[43,5,3,5,6,9],[43,5,3,5,6,9]]])
